c9a5963c7c92db21b11c47b5d5b886deee830d83
luandrey <lucas.andrey@gruyere-energie.ch>
2026-02-27 07:14:55 +0100

    clean project

---
+++ 2_Program/run_Filter_Techant.bat
@@ -0,0 +1,4 @@
+@echo off
+cd /d "%~dp0"
+.venv\Scripts\python.exe "0_Data\Filter_Techant.py"
+pause
--- run_Filter_Techant.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-cd /d "%~dp0"
-.venv\Scripts\python.exe "2_Program\0_Data\Filter_Techant.py"
-pause
